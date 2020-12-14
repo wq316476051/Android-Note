8e9d770e8ddddf3460305a50e529340f98503614
wq316476051 <316476051@qq.com>
2020-12-14 20:23:16 +0800

    Create 06.Limit 子句.mk

---
+++ 04.数据存储/03.数据库 SQLite/06.Limit 子句.mk
@@ -0,0 +1,11 @@
+## LIMIT 子句用于限制由 SELECT 语句返回的数据数量。（列表分页）
+
+```
+SELECT column1, column2, columnN 
+FROM table_name
+LIMIT [no of rows];
+
+SELECT column1, column2, columnN 
+FROM table_name
+LIMIT [no of rows] OFFSET [row num]
+```
